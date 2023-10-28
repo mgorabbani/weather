6874fbeda73dec8a0843fa4c81c87890e866c31c
mgorabbani <mgorabbani@gmail.com>
2023-10-28 22:52:18 +0200

    add favourite functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
 import WeatherDisplay from "./components/WeatherDisplay";
 
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d956fa0dc4a306ea064eba4cb1a68c35
 const App: React.FC = () => {
   const [city, setCity] = useState<any>();
+  const [favoriteCities, setFavoriteCities] = useState<any[]>([]);
 
+  const addFavoriteCity = (city: any) => {
+    setFavoriteCities([...favoriteCities, city]);
+  };
+  const removeFavoriteCity = (city: any) => {
+    const newFavoriteCities = favoriteCities.filter(
+      (favoriteCity) => favoriteCity.id !== city.id
+    );
+    setFavoriteCities(newFavoriteCities);
+  };
   return (
     <div className="app-container">
       <h1>Weather Application</h1>
+      <h2>Favorite Cities</h2>
+      <div>
+        {favoriteCities.map((favoriteCity, index) => (
+          <div key={index}>
+            <h3 onClick={() => setCity(favoriteCity)}>{favoriteCity.name}</h3>
+            <button onClick={() => removeFavoriteCity(favoriteCity)}>
+              Remove
+            </button>
+          </div>
+        ))}
+        <button onClick={() => addFavoriteCity(city)} disabled={!city}>
+          Add
+        </button>
+      </div>
       <SearchBar setCity={setCity} />
       <WeatherDisplay city={city} />
     </div>
